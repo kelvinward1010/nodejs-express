18e8e27f818f55b0ecae3738c38d57048ed3cfdb
kelvinward1010 <kelvinward1010@gmail.com>
2023-03-03 16:13:50 +0700

    update

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,5 @@
 const Post = require('../models/post-model');
 
-
 const createPost = async (req, res, next) => {
     try {
         const newPost = new Post(req.body);
@@ -11,10 +10,24 @@ const createPost = async (req, res, next) => {
     }
 }
 
+// update anyway
+const updatePostAnyWay = async (req, res, next) => {
+    try {
+        const postUpdate = await Post.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true});
+        res.status(201).json(postUpdate)
+    } catch (error) {
+        res.status(500).json(error)
+        next(error)
+    }
+}
+
+//update follow username
 const updatePost = async (req, res, next) => {
     try {
         const post = await Post.findById(req.params.id);
-        if(post.username == req.body.name) {
+        if(post.username == req.body.username) {
             try {
                 const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
                     $set: req.body
@@ -32,32 +45,71 @@ const updatePost = async (req, res, next) => {
     }
 }
 
-const deletePost = async (req, res, next) => {
+
+// delete anyway
+const deletePostAnyWay = async (req, res, next) => {
     try {
-        
+        const post = await Post.findById(req.params.id);
+        const deletePost = await post.delete();
+        res.status(200).json(deletePost)
+        console.log("Post delete by delete anyway")
     } catch (error) {
-        
+        res.status(500).json(error)
+        next(error)
     }
 }
 
+// delete follow username
+const deletePost = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.username == req.body.username){
+            try {
+                await post.delete();
+                res.status(200).json("Post has been delete!")
+            } catch (error) {
+                res.status(500).json(error)
+            }
+        }
+    } catch (error) {
+        res.status(500).json(error)
+        next(error)
+    }
+}
+
+
 const getPost = async (req, res, next) => {
     try {
-        
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post)
     } catch (error) {
-        
+        res.status(500).json(error);
+        next(error)
     }
 }
 
 const getAllPost = async (req, res, next) => {
+    const username = req.query.user;
     try {
-        
+        let posts;
+        if(username){
+            posts = await Post.find({username})
+        }else{
+            posts = await Post.find()
+        }
+        res.status(200).json(posts)
     } catch (error) {
-        
+        res.status(500).json(error)
+        next(error)
     }
 }
 
 module.exports = {
     createPost,
+    updatePostAnyWay,
     updatePost,
+    deletePostAnyWay,
     deletePost,
+    getPost,
+    getAllPost,
 }
--- routes/post-route.js
+++ routes/post-route.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { createPost, updatePost } = require('../controllers/post');
+const { createPost, updatePost, deletePostAnyWay, updatePostAnyWay, deletePost, getPost, getAllPost } = require('../controllers/post');
 
 
 const router = express.Router();
 
 router.post('/create-post', createPost);
-router.put('/update-post/:id', updatePost);
+router.put('/update-post/:id', updatePostAnyWay);
+router.put('/update-post-follow-user/:id', updatePost);
+router.delete('/delete-post/:id', deletePostAnyWay);
+router.delete('/delete-post-follow-user/:id', deletePost);
+router.get('/post/:id', getPost);
+router.get('/allpost', getAllPost)
 
 module.exports = router;
